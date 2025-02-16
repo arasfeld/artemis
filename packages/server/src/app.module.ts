221b189a895759af97bca6f9e42a6249c0f5e30d
Adam Rasfeld <arasfeld@gmail.com>
2025-02-16 10:12:05 -0500

    add mikroorm & inital schema

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '../../.env' }),
+    DatabaseModule.register(),
+  ],
 })
 export class AppModule {}
